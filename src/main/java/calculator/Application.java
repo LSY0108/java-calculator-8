574433ee3f33d99ea88ae04a2c03096fe429cd79
LSY0108 <seungye1209@gmail.com>
2025-10-14 22:59:32 +0900

    feat(add): split numbers by comma or colon and calculate sum

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,5 +1,8 @@
 package calculator;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
@@ -10,13 +13,31 @@ public class Application {
     }
 
     public static int add(String input) {
-
         if(input == null || input.isEmpty()) {
             return 0;
         }
 
+        String[] numArray = null;
 
-        return -1;
+        if(!input.startsWith("//")){
+            String[] commaSplit = input.split(",");
+            List<String> numStr = new ArrayList<>();
+
+            for(String num : commaSplit){
+                String[] colonSplit = num.split(":");
+                for(String t : colonSplit){
+                    numStr.add(t);
+                }
+            }
+            numArray = numStr.toArray(new String[0]);
+        }
+
+        // 숫자 합산
+        int sum = 0;
+        for(String n : numArray){
+            int number = Integer.parseInt(n);
+            sum += number;
+        }
+        return sum;
     }
-
 }
